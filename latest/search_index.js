9bbba0f4bb5d9c178c27016a0d054bab4df9f696
autodocs <autodocs>
2018-05-03 18:56:48 +0000

    build based on b3f5d9c

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.abs_rev",
     "category": "method",
-    "text": "Reverse abs\n\n\n\n\n\n"
+    "text": "Reverse abs\n\n\n\n"
 },
 
 {
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.asin_rev",
     "category": "method",
-    "text": "Reverse asin.\n\n\n\n\n\n"
+    "text": "Reverse asin.\n\n\n\n"
 },
 
 {
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.div_rev",
     "category": "method",
-    "text": "Reverse division\n\n\n\n\n\n"
+    "text": "Reverse division\n\n\n\n"
 },
 
 {
@@ -61,7 +61,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.minus_rev",
     "category": "method",
-    "text": "Reverse minus\n\n\n\n\n\n"
+    "text": "Reverse minus\n\n\n\n"
 },
 
 {
@@ -69,7 +69,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.mul_rev",
     "category": "method",
-    "text": "Reverse multiplication\n\n\n\n\n\n"
+    "text": "Reverse multiplication\n\n\n\n"
 },
 
 {
@@ -77,7 +77,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.mul_rev_IEEE1788",
     "category": "method",
-    "text": "According to the IEEE-1788 standard:\n\n∘_rev1(b, c, x) is the subset of x such that x ∘ b is defined and in c;\n∘_rev2(a, c, x) is the subset of x such that a ∘ x is defined and in c\n\nWhen ∘ is commutative, these agree and we write ∘_rev(b, c, x).\n\n\n\n\n\n"
+    "text": "According to the IEEE-1788 standard:\n\n∘_rev1(b, c, x) is the subset of x such that x ∘ b is defined and in c;\n∘_rev2(a, c, x) is the subset of x such that a ∘ x is defined and in c\n\nWhen ∘ is commutative, these agree and we write ∘_rev(b, c, x).\n\n\n\n"
 },
 
 {
@@ -85,7 +85,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.plus_rev",
     "category": "method",
-    "text": "Reverse plus\n\n\n\n\n\n"
+    "text": "Reverse plus\n\n\n\n"
 },
 
 {
@@ -93,7 +93,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.power_rev",
     "category": "method",
-    "text": "Reverse power\n\n\n\n\n\n"
+    "text": "Reverse power\n\n\n\n"
 },
 
 {
@@ -109,7 +109,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.sqr_rev",
     "category": "method",
-    "text": "Reverse sqr\n\n\n\n\n\n"
+    "text": "Reverse sqr\n\n\n\n"
 },
 
 {
@@ -117,7 +117,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.sqrt_rev",
     "category": "method",
-    "text": "Reverse square root\n\n\n\n\n\n"
+    "text": "Reverse square root\n\n\n\n"
 },
 
 {
@@ -133,7 +133,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.reverse_operations",
     "category": "constant",
-    "text": "Dictionary mapping functions to their reverse functions.\n\n\n\n\n\n"
+    "text": "Dictionary mapping functions to their reverse functions.\n\n\n\n"
 },
 
 {
@@ -149,7 +149,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.cos_main",
     "category": "method",
-    "text": "Contractor for main branch of cos, from x = 0 to π.\n\n\n\n\n\n"
+    "text": "Contractor for main branch of cos, from x = 0 to π.\n\n\n\n"
 },
 
 {
@@ -165,7 +165,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.periodise",
     "category": "method",
-    "text": "Periodize the contractor C\n\n\n\n\n\n"
+    "text": "Periodize the contractor C\n\n\n\n"
 },
 
 {
@@ -173,7 +173,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.reflect_x",
     "category": "method",
-    "text": "Reflect in mirror at position x_mirror\n\n\n\n\n\n"
+    "text": "Reflect in mirror at position x_mirror\n\n\n\n"
 },
 
 {
@@ -189,7 +189,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.sin_main",
     "category": "method",
-    "text": "Contractor for \"main branch\" of sin, from x = -π/2 to π/2.\n\n\n\n\n\n"
+    "text": "Contractor for \"main branch\" of sin, from x = -π/2 to π/2.\n\n\n\n"
 },
 
 {
@@ -197,7 +197,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.square_pos",
     "category": "method",
-    "text": "Contractor for y = x^2, x >= 0\n\n\n\n\n\n"
+    "text": "Contractor for y = x^2, x >= 0\n\n\n\n"
 },
 
 {
@@ -205,7 +205,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.symmetrise",
     "category": "method",
-    "text": "Symmetric part of a Contractor, via an involution op (i.e. such that inv(op) == op).\n\n\n\n\n\n"
+    "text": "Symmetric part of a Contractor, via an involution op (i.e. such that inv(op) == op).\n\n\n\n"
 },
 
 {
@@ -213,7 +213,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.tan_main",
     "category": "method",
-    "text": "Contractor for \"main branch\" of tan, from x = -π/2 to π/2.\n\n\n\n\n\n"
+    "text": "Contractor for \"main branch\" of tan, from x = -π/2 to π/2.\n\n\n\n"
 },
 
 {
@@ -221,7 +221,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.translate",
     "category": "method",
-    "text": "Translation of a Contractor C by α. Uses inv(op) ∘ C ∘ op\n\n\n\n\n\n"
+    "text": "Translation of a Contractor C by α. Uses inv(op) ∘ C ∘ op\n\n\n\n"
 },
 
 {
@@ -229,7 +229,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IntervalContractors.translate",
     "category": "method",
-    "text": "translate(α)\n\nReturns a function that shifts (translates) a 2D IntervalBox in x (the first coordinate).\n\n\n\n\n\n"
+    "text": "translate(α)\n\nReturns a function that shifts (translates) a 2D IntervalBox in x (the first coordinate).\n\n\n\n"
 },
 
 {
